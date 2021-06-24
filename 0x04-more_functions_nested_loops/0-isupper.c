4abc79e310d0b23d82ac28f9a51142bd0ebdae56
Annietah <annemwangi32@gmail.com>
2021-06-24 05:30:08 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _isupper - Entry point
  *
- *@c: Character to check if it's alphabet
+ * @c: Character to check if it's alphabet
  * Return: Always 0 (success)
  */
 int _isupper(int c)
 {
-  if((c <= 65 && (c  <= 122))
+  if(c >= 65 && c  <= 90)
     {
       return (1);
     }
