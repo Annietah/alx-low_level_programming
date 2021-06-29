d43c98dd8cf9219f7a954eeb5ec2b7a39d7c2c18
Annietah <annemwangi32@gmail.com>
2021-06-29 10:51:32 -0700

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-  int i;
-  char n[9] = "Holberton";
-  
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(n[i]);
-    };
-  _putchar('\n');
-  return (0);
+int i;
+char n[9] = "Holberton";
+for (i = 0; i < 9; i++)
+{
+_putchar(n[i]);
+}
+_putchar('\n');
+return (0);
 }
