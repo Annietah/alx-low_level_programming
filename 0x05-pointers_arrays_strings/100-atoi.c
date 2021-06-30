ecf2a6c1b66db8c627fa7e4c594bc2f166f6eb93
Annietah <annemwangi32@gmail.com>
2021-06-29 22:24:46 -0700

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,7 @@ int _atoi(char *s)
 int sign = 1;
 unsigned int num = 0;
 
-do
-{
+do{
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
@@ -20,7 +19,6 @@ num = (num * 10) + (*s - '0');
 else if (num > 0)
 break;
 
-}
-while (*s++);
+}while (*s++);
 return (num *sign);
 }
