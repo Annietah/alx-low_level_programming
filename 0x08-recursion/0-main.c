95ff8396469795504d4efab618f5e0673b42d539
Annietah <annemwangi32@gmail.com>
2021-07-07 22:24:54 +0300

    Recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,13 @@
+julien@ubuntu:~/0x08. Recursion$ cat 0-main.c
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Betty Holberton");
+    return (0);
+}
