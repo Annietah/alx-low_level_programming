2aa05117328ae7676144d8ed2db74bfc16d3556b
Annietah <annemwangi32@gmail.com>
2021-07-10 22:06:07 +0300

    if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print an alphabet
+*Return: 0 (success)
+*/
+int main(void)
+{
+char l;
+
+for (l = 'a' ; l <= 'z' ; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
