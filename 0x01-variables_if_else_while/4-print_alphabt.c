caa10b3e8156a09811e3a92c7a34c4c783f28f54
Annietah <annemwangi32@gmail.com>
2021-07-10 22:12:36 +0300

    if else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,23 @@
-include <stdio.h>
+# include <stdio.h>
+
 /**
-* main - Print an alphabet without q and e
-*Return: 0 (success)
-*/
+ *main - beginning of program
+ *
+ *Description: prints the alphabet followed by a blank line but will not print
+ *'q' or 'e'
+ *
+ *Return: 0
+ */
 int main(void)
 {
-char l;
+char abc;
 
-for (l = 'a' ; l <= 'z' ; l++)
+for (abc = 'a'; abc <= 'z'; abc++)
 {
-if (l != 'q' && l != 'e')
-putchar(l);
+if (abc != 'e' && abc != 'q')
+{
+putchar(abc);
+}
 }
 putchar('\n');
 return (0);
